13a959be6690986edf2de03e24bf6f4a2580495d
Luís Ângelo Rodrigues Jr <luisangelorjr@gmail.com>
2017-10-27 16:54:40 -0200

    Update NumerosPrimos.java

--- NumerosPrimos.java
+++ NumerosPrimos.java
@@ -1,1 +1,26 @@
 //Criado por Luís Ângelo Rodrigues Jr. em 20171027 - 16h37
+
+import java.util.Scanner;
+
+public class NumerosPrimos {
+  public static void main (String [] args) {
+    
+    int num, i, count=0;
+    
+    Scanner scan = new Scanner (System.in);
+        System.out.print("Insira um número: ");
+        num = scan.nextInt();
+    
+    for (i=2; i<num; i++) {
+      if (num % i == 0){
+        count++;
+        break;
+      }
+	}
+	if (count==0){
+	  System.out.print("Esse é um número primo: ");
+	}else{
+	  System.out.print("Esse não é um número primo: ");
+	  }
+	}
+}
